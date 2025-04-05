5641d76c7b1bb51f702eb661e23acdf20d8025b8
Kesley Rana <kesley_rana@sfu.ca>
2025-04-05 10:58:09 -0700

    First commit, using Tkinter library.

---
+++ main.py
@@ -0,0 +1,6 @@
+from tkinter import *
+
+root = Tk() #using root as a variable is standard practice when using tkinter
+root.geometry('1440x720')
+root.title('Mine Sweeper')
+root.mainloop() #this is needed to keep the window running
